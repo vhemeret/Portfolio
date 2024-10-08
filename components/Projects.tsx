cda791409e028aa630cde5808883271eab69d9ae
Vhemeret <val.kawa@icloud.com>
2024-10-08 18:08:14 +0200

    description modal done

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,6 +4,8 @@ import { useRef } from 'react'
 import Irc from '../pages/img/ft_irc.webp'
 import Inception from '../pages/img/inception.webp'
 import Chinoflix from '../pages/img/Chinoflix.webp'
+import SoLong from '../pages/img/solong.webp'
+import Minishell from '../pages/img/minishell.webp'
 import Transcendence from '../pages/img/ft_transcendence.webp'
 import Image, { StaticImageData } from 'next/image'
 import { ThreeDCardDemo } from './3d-card'
@@ -15,7 +17,6 @@ interface projectInterface {
 	description: string;
 	fullDescription: string;
 	technologies: string;
-	goal: string;
 	mainImage: StaticImageData;
 	additionalImage: StaticImageData[];
 }
@@ -82,22 +83,20 @@ function Projects() {
 		{
 			id: 1,
 			title: 'Netflix Clone',
-			description: "Réplique des fonctionnalités principales de Netflix.",
-			fullDescription: "Ce clone de Netflix est une démonstration complète de compétences en développement full-stack. Construit avec React pour le front-end, il offre une interface utilisateur fluide et responsive, mimant l'expérience de navigation de Netflix. Le back-end, développé avec Node.js et Express, gère l'authentification des utilisateurs, le stockage des données dans MongoDB, et sert une API RESTful pour la communication avec le front-end. Le projet inclut des fonctionnalités telles que la recherche de contenu, la lecture vidéo, la gestion des profils utilisateurs, et des recommandations personnalisées basées sur l'historique de visionnage.",
+			description: "Réplique simple de l'interface utilisateur de Netflix.",
+			fullDescription: "Ce clone de Netflix est une démonstration approfondie des compétences en développement front-end. Construit avec Next.js, il offre une interface utilisateur fluide et responsive, reproduisant fidèlement l'expérience de navigation de Netflix. Le projet utilise l'API TMDB pour récupérer les données des films et des séries TV, permettant ainsi d'afficher un contenu réel et à jour. Les fonctionnalités incluent l'affichage des films et série et la visualisation des détails des contenus. Ce projet met en évidence la capacité à travailler avec des API externes, à gérer l'état de l'application, et à créer une interface utilisateur moderne et interactive.",
 			mainImage: Chinoflix,
 			additionalImage: [],
-			technologies: "React, Node.js, Express, MongoDB, API RESTful",
-			goal: "Créer une plateforme de streaming vidéo similaire à Netflix pour démontrer des compétences en développement full-stack."
+			technologies: "#Next.js #TMDB-API #React"
 		},
 		{
 			id: 2,
 			title: 'ft_transcendence',
 			description: "Plateforme de jeu en ligne multijoueur avec chat intégré.",
-			fullDescription: "ft_transcendence est une plateforme de jeu en ligne multijoueur centrée autour du jeu classique Pong. Développée avec Vue.js pour le front-end et NestJS pour le back-end, l'application offre une expérience de jeu en temps réel grâce à l'utilisation de WebSocket. Les joueurs peuvent participer à des matchs, communiquer via un système de chat intégré, et suivre leurs statistiques de jeu. L'application inclut également des fonctionnalités sociales comme l'ajout d'amis et la création de tournois. Avec une base de données PostgreSQL pour le stockage des données utilisateur et des scores, ft_transcendence démontre la capacité à construire une application web full-stack complexe et interactive.",
+			fullDescription: "ft_transcendence est une plateforme de jeu en ligne multijoueur centrée autour du jeu classique Pong. Développée avec React pour le front-end et NestJS pour le back-end, l'application offre une expérience de jeu en temps réel grâce à l'utilisation de WebSocket. Les joueurs peuvent participer à des matchs, communiquer via un système de chat, et suivre leurs statistiques de jeu. L'application inclut également des fonctionnalités sociales comme l'ajout d'amis et l'invitation en partie. Avec une base de données PostgreSQL pour le stockage des données utilisateur et des scores.",
 			mainImage: Transcendence,
 			additionalImage: [],
-			technologies: "Vue.js, NestJS, PostgreSQL, WebSocket",
-			goal: "Développer une application web complexe intégrant un jeu multijoueur en temps réel, un système de chat, et des fonctionnalités sociales."
+			technologies: "#React #NestJS #PostgreSQL #WebSocket #Docker"
 		},
 		{
 			id: 3,
@@ -106,8 +105,7 @@ function Projects() {
 			fullDescription: "Inception est un projet d'infrastructure qui utilise Docker pour créer un environnement de déploiement virtualisé pour des applications web. L'infrastructure comprend des conteneurs séparés pour NGINX (serveur web), MariaDB (base de données), et WordPress (application), tous orchestrés via Docker Compose. Ce projet met l'accent sur la sécurité, la performance et la portabilité, permettant un déploiement rapide et cohérent d'environnements de développement et de production. Il démontre la maîtrise des concepts de virtualisation, de la gestion des conteneurs, et des meilleures pratiques en matière d'architecture de microservices.",
 			mainImage: Inception,
 			additionalImage: [],
-			technologies: "Docker, NGINX, MariaDB, WordPress, Docker Compose",
-			goal: "Créer une infrastructure virtualisée pour héberger des applications web avec une configuration sécurisée et hautement disponible."
+			technologies: "#Docker #Nginx #MariaDB #WordPress #Docker-Compose"
 		},
 		{
 			id: 4,
@@ -116,8 +114,25 @@ function Projects() {
 			fullDescription: "ft_irc est une implémentation personnalisée d'un serveur IRC (Internet Relay Chat) en C++. Ce projet explore en profondeur les concepts de programmation réseau, utilisant des sockets TCP/IP pour gérer les connexions client-serveur. Il implémente le protocole IRC, permettant aux utilisateurs de se connecter, de rejoindre des canaux de discussion, et d'échanger des messages en temps réel. Le serveur gère plusieurs connexions simultanées grâce à une architecture multithreadée, démontrant une compréhension avancée de la programmation concurrente. ft_irc met en lumière les défis de la gestion d'état, de la synchronisation, et de la performance dans les applications réseau.",
 			mainImage: Irc,
 			additionalImage: [],
-			technologies: "C++, Sockets TCP/IP, Multithreading",
-			goal: "Implémenter un serveur IRC fonctionnel pour comprendre les protocoles de communication en réseau et la programmation concurrente."
+			technologies: "#C++ #Sockets TCP/IP #Multithreading"
+		},
+		{
+			id: 5,
+			title: 'Minishell',
+			description: "Implémentation d'un shell simple, similaire à bash.",
+			fullDescription: "Minishell est un projet qui consiste à créer un interpréteur de commandes simple, similaire à bash. Ce projet approfondit la compréhension des processus et des descripteurs de fichiers dans les systèmes Unix. Il implémente des fonctionnalités essentielles telles que l'affichage d'un prompt, la gestion de l'historique des commandes, la recherche et le lancement d'exécutables, la gestion des guillemets simples et doubles, les redirections et les pipes, la gestion des variables d'environnement, et le comportement des signaux. Le projet inclut également l'implémentation de commandes intégrées comme echo, cd, pwd, export, unset, env, et exit.",
+			mainImage: Minishell,
+			additionalImage: [],
+			technologies: "#C #Unix"
+		},
+		{
+			id: 6,
+			title: 'SoLong',
+			description: "Jeu 2D inspiré de \"The Hitchhiker's Guide to the Galaxy\".",
+			fullDescription: "So Long est un projet de jeu 2D simple conçu pour explorer l'utilisation de textures, de sprites et d'éléments de gameplay de base. Inspiré par \"Le Guide du voyageur galactique\", ce jeu met en scène un dauphin essayant de s'échapper de la Terre après avoir mangé des poissons. Le jeu comprend la génération de cartes à partir de fichiers .ber, des mouvements fluides du personnage (W, A, S, D), la collecte d'objets, la recherche de sortie, la détection de collision avec les murs et un affichage graphique utilisant la bibliothèque MiniLibX. Ce projet démontre la compréhension des concepts de base du développement de jeux 2D, y compris la gestion des graphismes, la logique de jeu et l'interaction utilisateur.",
+			mainImage: SoLong,
+			additionalImage: [],
+			technologies: "#C #MiniLibX"
 		},
 	];
 
@@ -154,8 +169,9 @@ function Projects() {
 								<Image className='rounded-2xl mb-3 z-50' src={selectedProject.mainImage} alt={selectedProject.title} />
 								<h1 className='font-bold text-xl ml-2 mb-2'>{selectedProject.title}</h1>
 								<p className='font-thin text-sm ml-2'>{selectedProject.fullDescription}</p>
+								<p className='font text-sm ml-2 mt-4 tracking-widest text-violet-400'>{selectedProject.technologies}</p>
 								<div className='flex justify-center'>
-									<button onClick={() => setIsOpen(false)} className='md:bg-gray-800 md:hover:bg-black-200 px-2 py-1 rounded-xl mt-4 font-thin text-gray-500 hover:text-gray-600 transition-all cursor-pointer'>fermer</button>
+									<button onClick={() => setIsOpen(false)} className=' px-2 py-1 rounded-xl mt-4 text-gray-500 hover:text-gray-600 transition-all cursor-pointer'>fermer</button>
 								</div>
 							</div>
 						</div>
