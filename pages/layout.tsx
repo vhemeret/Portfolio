3ba47e940afbad4168f0445a16f6064a2c70977b
Vhemeret <val.kawa@icloud.com>
2024-10-11 14:20:29 +0200

    new cv

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -5,25 +5,25 @@ import { Analytics } from '@vercel/analytics/react';
 
 interface RootLayoutProps {
 	children: ReactNode
-  }
+}
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  return (
-    <>
-      <html lang="en" suppressHydrationWarning>
-        <head />
-        <body>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-		  <Analytics />
-        </body>
-      </html>
-    </>
-  )
+	return (
+		<>
+			<html lang="en" suppressHydrationWarning>
+				<head />
+				<body>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+					>
+						{children}
+						<Analytics />
+					</ThemeProvider>
+				</body>
+			</html>
+		</>
+	)
 }
